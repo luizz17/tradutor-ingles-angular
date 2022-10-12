452180e5600ea5968bf48f2cdea7d2858f90bc9a
luizz17 <luizz.camara@outlook.com.br>
2022-10-12 07:36:00 -0300

    Verificação de tradução

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -14,14 +14,38 @@ export class PainelComponent implements OnInit {
   public instrucao: string = 'Traduza a frase:'
   public resposta!: string;
 
-  constructor() {console.log(this.frases)}
+  public rodada: number = 0;
+  public rodadaFrase!: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)}
 
   ngOnInit(): void {
   }
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-    console.log(this.resposta)
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert('A tradução está correta!!')
+
+      //Trocar pergunt da rodada
+      this.rodada++
+
+      //Atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada]
+
+    }else{
+      alert('A tradução está incorreta!!\nTente novamente!!')
+    }
+
+    
+
   }
 
 }
